f830a1594c02c743cb8958854749dc344ffdf80a
jo8412368 <120777738+jo8412368@users.noreply.github.com>
2024-06-07 09:58:00 -0400

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -53,7 +53,6 @@ ${tradutor.texto1[9]} ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌')
   
   ▢ _${usedPrefix}menuaudios_
   ▢ _${usedPrefix}menuanimes_
-  ▢ _${usedPrefix}labiblia_ (🔞)
   ▢ _${usedPrefix}lang_ ${tradutor.texto2}
   ▢ _${usedPrefix}langgroup_ ${tradutor.texto3}
   ▢ _${usedPrefix}glx_ ${tradutor.texto4 /** Este menú permanecerá aquí por un corto tiempo, hasta que la gente sepa */}
@@ -107,7 +106,6 @@ ${tradutor.texto1[9]} ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌')
   ▢ _${usedPrefix}ppt *<papel / tijera /piedra>*_
   ▢ _${usedPrefix}prostituto *<nombre / @tag>*_
   ▢ _${usedPrefix}prostituta *<nombre / @tag>*_
-  ▢ _${usedPrefix}gay2 *<nombre / @tag>*_
   ▢ _${usedPrefix}lesbiana *<nombre / @tag>*_
   ▢ _${usedPrefix}pajero *<nombre / @tag>*_
   ▢ _${usedPrefix}pajera *<nombre / @tag>*_
@@ -203,8 +201,6 @@ ${tradutor.texto1[9]} ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌')
  ▢ _${usedPrefix}stickerpack *<url>*_
  ▢ _${usedPrefix}wallpaper *<txt>*_ 
  ▢ _${usedPrefix}dapk2 *<url>*_
- ▢ _${usedPrefix}xnxxdl *<url>*_ (🔞)
- ▢ _${usedPrefix}xvideosdl *<url>*_ (🔞)
 
 
  ${tradutor.texto1[16]}
@@ -214,7 +210,6 @@ ${tradutor.texto1[9]} ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌')
   ▢ _${usedPrefix}modapk *<txt>*_
   ▢ _${usedPrefix}stickersearch *<txt>*_
   ▢ _${usedPrefix}stickersearch2 *<txt>*_
-  ▢ _${usedPrefix}xnxxsearch *<txt>*_
   ▢ _${usedPrefix}animeinfo *<txt>*_
   ▢ _${usedPrefix}google *<txt>*_
   ▢ _${usedPrefix}letra *<txt>*_
